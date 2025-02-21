1276dabe5ac574aeeae81aaddd83cca5b72ec49d
Andrew Arneson <Loran425@proton.me>
2025-02-20 19:11:50 -0700

    Add advanced_search modal window

--- popups/__init__.py
+++ popups/__init__.py
@@ -2,5 +2,6 @@ from .base_popup import BasePopup
 from .about import AboutPopup
 from .notes_export import ExportPopup
 from .notes_import import ImportPopup
+from .advanced_search import AdvancedSearchPopup
 
-__all__ = ["BasePopup", "AboutPopup", "ExportPopup", "ImportPopup"]
+__all__ = ["BasePopup", "AboutPopup", "ExportPopup", "ImportPopup", "AdvancedSearchPopup"]
